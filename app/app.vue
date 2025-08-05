058d91e6a9b7735175354b400aaae037909ba36d
elliahsieh <ellia.hsieh@gmail.com>
2025-08-06 02:29:19 +0800

    Install Tailwind CSS and integrate with Nuxt

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen bg-white flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Hey I'm Ellia! Welcome to my page 🎉
+    </h1>
   </div>
 </template>
