b2104d989c2e00ea69bc885ee23e39270299e8bd
elliahsieh <ellia.hsieh@gmail.com>
2025-08-06 02:59:04 +0800

    Final debug state

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="min-h-screen bg-white flex items-center justify-center">
-    <h1 class="text-4xl font-bold text-blue-600">
-      Hey I'm Ellia! Welcome to my page 🎉
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ content/blog/my-first-post.md
@@ -0,0 +1,8 @@
+---
+title: My First Post
+description: Testing if Nuxt content routing works
+---
+
+# My First Post
+
+If you can read this, it’s working 🎉
